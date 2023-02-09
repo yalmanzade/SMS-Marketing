7533bd63f4b7b525953dc776da5d9cad18d4bf2a
I-lluster <113929219+I-lluster@users.noreply.github.com>
2023-02-09 10:32:46 -0600

    Facebook API Work
    Facebook command line api work and a web app to post to a page.

---
+++ Facebook NET CORE/PostFacebook/PostFacebook/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostFacebook.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string accessToken, string pageId, string message)
+        {
+            var url = $"https://graph.facebook.com/v16.0/{pageId}/feed?access_token={accessToken}";
+            var data = $"message={message}";
+            var dataBytes = Encoding.UTF8.GetBytes(data);
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, new ByteArrayContent(dataBytes));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["Result"] = "Post was successful.";
+                }
+                else
+                {
+                    TempData["Result"] = "Post was unsuccessful.";
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
