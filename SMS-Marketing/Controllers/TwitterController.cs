c4c45f9dfcb48de9bd9508ed6d6cbfce6e3a690c
Yosep <yosepalmanza@hotmail.com>
2023-02-13 21:23:34 -0600

    Deleted keys

--- SMS-Marketing/Controllers/TwitterController.cs
+++ SMS-Marketing/Controllers/TwitterController.cs
@@ -19,8 +19,8 @@ namespace SMS_Marketing.Controllers
             {
                 CredentialStore = new SessionStateCredentialStore(HttpContext.Session)
                 {
-                    ConsumerKey = "SDyA6NEPZijc5Dru03t2l7H9p",
-                    ConsumerSecret = "10v0HH6SH9XOmvoKOeTLSVLl1EZeaZ8bI6cH783SMycJqdTbLw"
+                    ConsumerKey = "",
+                    ConsumerSecret = ""
                 }
             };
             //await auth.CredentialStore.ClearAsync();
