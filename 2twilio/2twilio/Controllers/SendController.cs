b8b1952c06d644939b5d4d400f90823b2e173e6e
ross-5 <113264179+ross-5@users.noreply.github.com>
2023-02-23 23:36:43 -0600

    Twilio so far
    This is what I've worked on for twilio up to this point

---
+++ 2twilio/2twilio/Controllers/SendController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML.Messaging;
+
+namespace _2twilio.Controllers
+{
+    public class SendController : Controller
+    {
+        public IActionResult Send()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Send(string accountSid, string authToken, string fromNumber, string toNumber1, string url, string body)
+        {
+            TwilioClient.Init(accountSid, authToken);
+            if(url != null) 
+            {
+                var mediaUrl = new[] { new Uri(url) }.ToList();
+                var message = MessageResource.Create(
+                body: body,
+                from: new Twilio.Types.PhoneNumber(fromNumber),
+                mediaUrl: mediaUrl,
+                to: new Twilio.Types.PhoneNumber(toNumber1)
+                );
+                Console.WriteLine(message.Sid);
+            }
+            else
+            {
+                var message = MessageResource.Create(
+                body: body,
+                from: new Twilio.Types.PhoneNumber(fromNumber),
+                to: new Twilio.Types.PhoneNumber(toNumber1)
+                );
+                Console.WriteLine(message.Sid);
+            }
+            return RedirectToAction("Send");
+        }
+    }
+}
---
+++ 2twilio/2twilio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2twilio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
